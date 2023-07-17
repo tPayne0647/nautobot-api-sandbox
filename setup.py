d87dc4af54a0eb8ff5cba369ba08917a0d10afca
tpayne0647 <tristan.payne@northerndata.de>
2023-07-17 17:41:26 -0400

    added V1 of project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nautobot_api_sandbox',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'nautobot_api_sandbox=nautobot_api_sandbox.nautobot_api_sandbox_ui:user_interface',
+        ],
+    },
+)
