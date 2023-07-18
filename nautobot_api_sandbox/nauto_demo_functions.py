230b89dbe51f06eb91138cd97bcf04fd5a922327
tpayne0647 <tristan.payne@northerndata.de>
2023-07-17 23:58:35 -0400

    class refactor complete

--- nautobot_api_sandbox/nauto_demo_functions.py
+++ nautobot_api_sandbox/nauto_demo_functions.py
@@ -2,107 +2,92 @@ import requests
 import json
 
 
-# Nautobot Demo API sandbox
-
-# Demo URLs and Token
-base_url = "https://demo.nautobot.com/api/"
-sites_url = base_url + "dcim/sites/"
-devices_url = base_url + "dcim/devices/"
-tenant_url = base_url + "tenancy/tenants/"
-api_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-
-# Set Headers
-headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Authorization": f"Token {api_token}",
-}
-
-
-def show_sites():
-    global sites
-    data = requests.get(
-        sites_url,
-        headers=headers,
-    )
-    sites_data = data.json()
-
-    sites = [site["name"] for site in sites_data["results"]]
-    print(f"\nTotal sites: {len(sites)}\n \n{sites}")
-    return sites
-
-
-def show_devices(selected_site):
-    test_parameters = {"site": selected_site}
-    device_data = requests.get(
-        devices_url,
-        headers=headers,
-        params=test_parameters,
-    )
-    total_site_devices = device_data.json()["count"]
-    name_list = device_data.json()
-    device_names = [device["name"] for device in name_list["results"]]
-
-    print(
-        f"\nTotal number of devices in [{selected_site.upper()}]: {total_site_devices}\n\n{device_names}"
-    )
-    return total_site_devices
-
-
-tenant_id = ""
-
-
-def create_tenant(name):
-    global tenant_id
-    data = {
-        "name": name,
+class DemoNautobotClient:
+    def __init__(self, api_token):
+        self.base_url = "https://demo.nautobot.com/api/"
+        self.headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Token {api_token}",
     }
-    response = requests.post(tenant_url, headers=headers, data=json.dumps(data))
-
-    if response.status_code == 201:
-        print("Tenant created successfully.\n")
-        tenant_id = response.json().get("id")
+    
+    
+    def _get(self, url, params=None):
+        response = requests.get(url, headers=self.headers, params=params)
+        response.raise_for_status()
         return response.json()
-    else:
-        print("Failed to create tenant. Response:", response.content)
-        return None
+    
+    
+    def _post(self, url, data=None):
+        response = requests.post(url, headers=self.headers, json=data)
+        response.raise_for_status()
+        return response.json()
+    
+    def _delete(self, url):
+        response = requests.delete(url, headers=self.headers)
+        response.raise_for_status()
+        return response
+    
+    
+    def show_sites(self):
+        sites_url = self.base_url + "dcim/sites/"
+        data = self._get(sites_url)
+        sites = [site["name"] for site in data["results"]]
+        print(f"\nTotal sites: {len(sites)}\n \n{sites}")
+        return sites
+    
+    
+    def show_devices(self, selected_site):
+        devices_url = self.base_url + "dcim/devices/"
+        parameters = {"site": selected_site}
+        data = self._get(devices_url, params=parameters)
+        total_site_devices = data["count"]
+        device_names = [device["name"] for device in data["results"]]
 
+        print(f"\nTotal number of devices in [{selected_site.upper()}]: {total_site_devices}\n\n{device_names}")
+        return total_site_devices
+    
+    
+    def create_tenant(self, name):
+        tenant_url = self.base_url + "tenancy/tenants/"
+        data = {"name": name}
+        response = self._post(tenant_url, data=data)
+        
+        print("Tenant created successfully.\n")
+        tenant_id = response.get("id")
+        return response
+    
+    
+    def get_tenant_id(self, name):
+        tenant_url = self.base_url + "tenancy/tenants/"
+        data = self._get(tenant_url)
 
-def get_tenant_id(name):
-    response = requests.get(tenant_url, headers=headers)
-
-    if response.status_code == 200:
-        tenants = response.json()["results"]
+        tenants = data["results"]
         for tenant in tenants:
             if tenant["name"] == name:
                 print(tenant["id"])
                 return tenant["id"]
+
         print(f"Tenant with name '{name}' not found.")
         return None
-    else:
-        print(f"Failed to get tenants. Response: {response.content}")
-        return None
+    
+    
+    def delete_tenant(self, name):
+        tenant_id = self.get_tenant_id(name)
+        if tenant_id is None:
+            return
 
+        delete_tenant_url = self.base_url + "tenancy/tenants/" + tenant_id
+        self._delete(delete_tenant_url)
 
-def delete_tenant(name):
-    tenant_id = get_tenant_id(name)
-    if tenant_id is None:
-        return
-
-    delete_tenant_url = tenant_url + tenant_id
-    response = requests.delete(delete_tenant_url, headers=headers)
-
-    if response.status_code == 204:
         print(f"Tenant [{name}] deleted successfully!")
-    else:
-        print(f"ERROR! Tenant [{name}] could not be deleted")
+        
+        
+    def show_tenants(self):
+        tenant_url = self.base_url + "tenancy/tenants/"
+        data = self._get(tenant_url)
 
+        tenant_names = [tenant["name"] for tenant in data["results"]]
 
-def show_tenants():
-    tenant_data = requests.get(tenant_url, headers=headers)
-    total_tenants = tenant_data.json()["count"]
-    name_list = tenant_data.json()
-    tenant_names = [tenant["name"] for tenant in name_list["results"]]
-
-    print(f"\nTotal tenants: {len(tenant_names)}\n \n{tenant_names}")
-    return tenant_names
+        print(f"\nTotal tenants: {len(tenant_names)}\n \n{tenant_names}")
+        return tenant_names
