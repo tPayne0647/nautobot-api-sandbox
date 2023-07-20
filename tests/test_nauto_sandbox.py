bec1835b8ab687b647562a791d15ec5c49767a7e
tpayne0647 <tristan.payne@northerndata.de>
2023-07-20 13:32:41 -0400

    added additional pytest. misc refactor

--- tests/test_nauto_sandbox.py
+++ tests/test_nauto_sandbox.py
@@ -2,8 +2,9 @@
 Module for testing Nautobot API Sandbox
 """
 
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, Mock
 import pytest
+import pynautobot
 from nautobot_api_sandbox.nauto_demo_functions import (
     DemoNautobotClient,
     TenantNotFoundError,
@@ -46,87 +47,94 @@ def mock_token():
     return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
 
-@pytest.fixture
-def client(mock_api, mock_token):
-    """
-    Nautobot API client fixture for testing.
-    """
-    return DemoNautobotClient(api_token=mock_token, api=mock_api)
+class TestDemoNautobotClient:
+    @pytest.fixture(autouse=True)
+    def setup(self, mock_api, mock_token):
+        self.client = DemoNautobotClient(api_token=mock_token, api=mock_api)
+        self.mock_api = mock_api
 
+    def test_get_sites(self):
+        sites = self.client.get_sites()
+        assert len(sites) == 3
+        assert sites[0].name == "site1"
+        assert sites[1].name == "site2"
+        assert sites[2].name == "site3"
+        self.mock_api.dcim.sites.all.assert_called_once()
 
-def test_show_sites(client, mock_api):
-    """
-    Test case for showing sites.
-    """
-    sites = client.get_sites()
-    assert len(sites) == 3
-    assert sites[0].name == "site1"
-    assert sites[1].name == "site2"
-    assert sites[2].name == "site3"
+    def test_get_sites_no_sites(self):
+        self.mock_api.dcim.sites.all.return_value = []
+        sites = self.client.get_sites()
+        assert sites == []
 
+    def test_get_devices_no_devices(self):
+        self.mock_api.dcim.devices.filter.return_value = []
+        devices = self.client.get_devices(selected_site_name="site1")
+        assert devices == []
 
-def test_show_devices(client, mock_api):
-    """
-    Test case for showing devices.
-    """
-    devices = client.get_devices(selected_site_name="site1")
-    assert len(devices) == 3
-    assert devices[0].name == "device1"
-    assert devices[1].name == "device2"
-    assert devices[2].name == "device3"
+    def test_get_devices(self):
+        devices = self.client.get_devices(selected_site_name="site1")
+        assert len(devices) == 3
+        assert devices[0].name == "device1"
+        assert devices[1].name == "device2"
+        assert devices[2].name == "device3"
 
+    def test_create_tenant(self):
+        mock_tenant = MagicMock()
+        self.mock_api.tenancy.tenants.create.return_value = mock_tenant
+        tenant = self.client.create_tenant(name="Test Tenant")
+        assert tenant == mock_tenant
 
-def test_create_tenant(client, mock_api):
-    """
-    Test case for creating a tenant.
-    """
-    mock_tenant = MagicMock()
-    mock_api.tenancy.tenants.create.return_value = mock_tenant
-    tenant = client.create_tenant(name="Test Tenant")
-    assert tenant == mock_tenant
+    def test_create_existing_tenant(self):
+        mock_response = Mock()
+        mock_response.status_code = 400
+        mock_response.reason = "A tenant with this name already exists."
+        self.mock_api.tenancy.tenants.create.side_effect = pynautobot.core.query.RequestError(
+            mock_response
+        )
+        tenant = self.client.create_tenant(name="Test Tenant")
+        assert tenant is None
 
+    def test_get_tenant_existing(self):
+        mock_tenant = MagicMock()
+        mock_tenant.name = "Test Tenant"
+        mock_tenant.id = 123
+        self.mock_api.tenancy.tenants.get.return_value = mock_tenant
+        tenant = self.client.get_tenant(name="Test Tenant")
+        assert tenant.id == 123
+        assert tenant == mock_tenant
+        self.mock_api.tenancy.tenants.get.assert_called_once_with(name="Test Tenant")
 
-def test_get_tenant_existing(client, mock_api):
-    """
-    Test case for getting an existing tenant.
-    """
-    mock_tenant = MagicMock()
-    mock_tenant.name = "Test Tenant"
-    mock_tenant.id = 123
-    mock_api.tenancy.tenants.get.return_value = mock_tenant
-    tenant = client.get_tenant(name="Test Tenant")
-    assert tenant.id == 123
-    assert tenant == mock_tenant
-    mock_api.tenancy.tenants.get.assert_called_once_with(name="Test Tenant")
+    def test_get_tenant_nonexistent(self):
+        self.mock_api.tenancy.tenants.get.return_value = None
+        with pytest.raises(TenantNotFoundError):
+            self.client.get_tenant(name="Nonexistent Tenant")
+        self.mock_api.tenancy.tenants.get.assert_called_with(name="Nonexistent Tenant")
 
+    def test_delete_tenant_existing(self):
+        mock_tenant = MagicMock()
+        mock_tenant.name = "Test Tenant"
+        self.mock_api.tenancy.tenants.get.return_value = mock_tenant
+        self.client.delete_tenant(name="Test Tenant")
+        self.mock_api.tenancy.tenants.get.assert_called_once_with(name="Test Tenant")
+        mock_tenant.delete.assert_called_once()
 
-def test_get_tenant_nonexistent(client, mock_api):
-    """
-    Test case for getting a nonexistent tenant.
-    """
-    mock_api.tenancy.tenants.get.return_value = None
-    with pytest.raises(TenantNotFoundError):
-        client.get_tenant(name="Nonexistent Tenant")
-    mock_api.tenancy.tenants.get.assert_called_with(name="Nonexistent Tenant")
+    def test_delete_tenant_nonexistent(self):
+        self.mock_api.tenancy.tenants.get.return_value = None
+        with pytest.raises(TenantNotFoundError):
+            self.client.delete_tenant(name="Nonexistent Tenant")
+        self.mock_api.tenancy.tenants.get.assert_called_with(name="Nonexistent Tenant")
 
-
-def test_delete_tenant_existing(client, mock_api):
-    """
-    Test case for deleting an existing tenant.
-    """
-    mock_tenant = MagicMock()
-    mock_tenant.name = "Test Tenant"
-    mock_api.tenancy.tenants.get.return_value = mock_tenant
-    client.delete_tenant(name="Test Tenant")
-    mock_api.tenancy.tenants.get.assert_called_once_with(name="Test Tenant")
-    mock_tenant.delete.assert_called_once()
-
-
-def test_delete_tenant_nonexistent(client, mock_api):
-    """
-    Test case for deleting a nonexistent tenant.
-    """
-    mock_api.tenancy.tenants.get.return_value = None
-    with pytest.raises(TenantNotFoundError):
-        client.delete_tenant(name="Nonexistent Tenant")
-    mock_api.tenancy.tenants.get.assert_called_with(name="Nonexistent Tenant")
+    def test_delete_tenant_dependant(self):
+        mock_tenant = MagicMock()
+        mock_response = Mock()
+        mock_response.status_code = 409
+        mock_response.text = (
+            "The request failed with code 409 Conflict: "
+            "{'detail': 'Unable to delete object. 1 dependent objects were found: Policy 3 (88f1ce52-bd40-4780-95de-381c8ed14bc0)'}"
+        )
+        mock_tenant.delete.side_effect = pynautobot.core.query.RequestError(mock_response)
+        self.mock_api.tenancy.tenants.get.return_value = mock_tenant
+        success, message = self.client.delete_tenant(name="Test Tenant")
+        assert not success
+        assert "Failed to delete tenant" in message
+        mock_tenant.delete.assert_called_once()
